db51b84a0f1d948a2945790dc1a7e74d01f91d8c
DionCstS <dionathacastro1@gmail.com>
2024-10-11 15:43:51 -0300

    adcionado textfild e o botao

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button, TextField } from '@mui/material'
 
 export default function Form() {
   return (
     <div>
-        <h1>Teste Formulario</h1>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <Button variant="text">Text</Button>
     </div>
   )
 }
