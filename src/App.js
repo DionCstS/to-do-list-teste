2afb5bbde54100ee7b66399cbd219f4996fb3e19
DionCstS <dionathacastro1@gmail.com>
2024-10-12 11:28:40 -0300

    botao add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import Form from './components/Form'
-import TodoItem from './components/TodoItem';
-import { Container, List } from '@mui/material';
 import Home from './pages/Home';
 
 function App() {
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,7 +6,7 @@ export default function Form() {
     <Paper style={{ padding: "1em" }}>
         <div style={{ display:"flex", justifyContent:"center" }}>
             <TextField id="outlined-basic" label="Tarefa" variant="outlined" fullWidth/>
-            <Button variant="text">Text</Button>
+            <Button variant="text">ADD</Button>
         </div>
     </Paper>
   )
