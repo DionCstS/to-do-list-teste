d9ba14bf3d306361e26728135e0dfca0825bdc57
DionCstS <dionathacastro1@gmail.com>
2024-10-13 15:38:47 -0300

    criado arrary e funcao map para mapear vetor e retornar o componente item

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Paper, TextField } from '@mui/material'
 
 export default function Form() {
+  const [text, setText] = useState(null);
+
+
+
   return (
     <Paper style={{ padding: "1em" }}>
         <div style={{ display:"flex", justifyContent:"center" }}>
-            <TextField id="outlined-basic" label="Tarefa" variant="outlined" fullWidth/>
-            <Button variant="text">ADD</Button>
+            <TextField id="outlined-basic" label="Tarefa" variant="outlined" onChange={(e) => setText(e.target.value)} fullWidth/>
+            <Button variant="text" onClick={ () => console.log(text)}>ADD</Button>
         </div>
     </Paper>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,19 @@
 import { Container, List } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import TodoItem from '../components/TodoItem'
 import Form from '../components/Form'
 
 export default function Home() {
+  const [todos, setTodos] = useState([0,1,3]);
+ 
+
   return (
     <Container maxWidth="xs" style={{marginTop: "1rem"}}>
       <Form/>
-      <List sx={{ bgcolor: 'background.paper', marginTop: "1em"}}>
-        <TodoItem/>
+      <List sx={{ bgcolor: "background.paper", marginTop: "1em" }}>
+        {todos.map((todo) => (
+            <TodoItem/>
+        ))}
       </List>
     </Container>
   )
